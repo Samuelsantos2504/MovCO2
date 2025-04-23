a4a280f28aac24abb277daa254f15ab1b9e677c7
Samuelsantos2504 <158794345+Samuelsantos2504@users.noreply.github.com>
2025-04-23 21:11:08 +0000

    Eliminacion de formulario sobrante y separacion de estilos

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import {StyleSheet, Image, Text, TouchableOpacity, View, Dimensions} from 'react
 import {LinearGradient} from 'expo-linear-gradient';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
+import { styles } from './styles/HomeScreen.js'
 
 
 const { height } = Dimensions.get('window');
@@ -45,51 +46,3 @@ export default function HomeScreen () {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  safeArea: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-  image: {
-    width: '100%',
-    height: height * 0.68, 
-    opacity: 0.7, 
-    marginTop: 50,
-  },
-  buttonsContainer: {
-    width: '100%',
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  button: {
-    backgroundColor: 'rgba(0, 0, 0, 0.2)', 
-    width: '70%',
-    paddingVertical: 16,
-    borderRadius: 20,
-    alignItems: 'center',
-    marginBottom: 16,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontFamily: 'Montserrat',
-    fontWeight: 'bold',
-  },
-  butto: {
-    backgroundColor: '#FFFFFF',
-    width: '70%',
-    paddingVertical: 16,
-    borderRadius: 20,
-    alignItems: 'center',
-  },
-  buttonTex: {
-    fontFamily: 'Montserrat',
-    fontWeight: 'bold',
-    color: '#337C8D',
-    fontSize: 16,
-  },
-});
---
+++ Screens/styles/HomeScreen.js
@@ -0,0 +1,49 @@
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    safeArea: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+    image: {
+      width: '100%',
+      height: height * 0.68, 
+      opacity: 0.7, 
+      marginTop: 50,
+    },
+    buttonsContainer: {
+      width: '100%',
+      alignItems: 'center',
+      marginTop: 10,
+    },
+    button: {
+      backgroundColor: 'rgba(0, 0, 0, 0.2)', 
+      width: '70%',
+      paddingVertical: 16,
+      borderRadius: 20,
+      alignItems: 'center',
+      marginBottom: 16,
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 16,
+      fontFamily: 'Montserrat',
+      fontWeight: 'bold',
+    },
+    butto: {
+      backgroundColor: '#FFFFFF',
+      width: '70%',
+      paddingVertical: 16,
+      borderRadius: 20,
+      alignItems: 'center',
+    },
+    buttonTex: {
+      fontFamily: 'Montserrat',
+      fontWeight: 'bold',
+      color: '#337C8D',
+      fontSize: 16,
+    },
+  });
+  
